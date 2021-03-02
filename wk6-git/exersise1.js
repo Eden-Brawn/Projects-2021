d20533966a63e76ac00f328cbe42b3f3bb62df2e
Eden Brawn <edenbrawn@gmail.com>
2021-03-02 18:10:45 +0000

    clickdone

--- wk6-git/exersise1.js
+++ wk6-git/exersise1.js
@@ -9,15 +9,23 @@ function print5() {
 print5();
 print5();
 
-function header() {
-    var element = document.getElementById('idheader');
+function updateH1() {/*This function looks for the elements in html and then updates the contents of the tags their in*/
+    let element = document.getElementById('idheader');
     element.firstChild.nodeValue = 'NEW HEADING';
-
-    var element = document.getElementById('idp');
+}
+function updateParagraph() {/*This updates the paragraph to 'New paragraph' */
+    let element = document.getElementById('idp');
     element.firstChild.nodeValue = 'New paragraph';
 }
+function loadFunction() {/*This listens for when the header or paragraph has been clicked then calls updateH1 or updateParagraph */
+    let header = document.getElementById('idheader');
+    header.addEventListener('click', updateH1);
 
-document,addEventListener('click', header);
+    let paragraph = document.getElementById('idp');
+    paragraph.addEventListener('click', updateParagraph);
+}
+
+document.addEventListener('DOMContentLoaded', loadFunction); /*This listens for when the action in '' happens to call loadFunction */
 
 /*'DOMContentLoaded' listens for when content on the page has been loaded to call header
-    'click' listens for when a click on the page has happened to call header */
+    'click' listens for when a click anywhere on the page has happened to call header */
