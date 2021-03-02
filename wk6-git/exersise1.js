042e91bbc2eb01cdcf4744e27a3ce93cf45d0c84
Eden Brawn <edenbrawn@gmail.com>
2021-03-02 17:50:48 +0000

    clickelement

--- wk6-git/exersise1.js
+++ wk6-git/exersise1.js
@@ -8,3 +8,16 @@ function print5() {
 
 print5();
 print5();
+
+function header() {
+    var element = document.getElementById('idheader');
+    element.firstChild.nodeValue = 'NEW HEADING';
+
+    var element = document.getElementById('idp');
+    element.firstChild.nodeValue = 'New paragraph';
+}
+
+document,addEventListener('click', header);
+
+/*'DOMContentLoaded' listens for when content on the page has been loaded to call header
+    'click' listens for when a click on the page has happened to call header */
