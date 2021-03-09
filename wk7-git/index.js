521c22d4f5f4f5fffa1abae20f1a0752806ce7b4
Eden Brawn <edenbrawn@gmail.com>
2021-03-09 17:01:05 +0000

    EX1AND2

---
+++ wk7-git/index.js
@@ -0,0 +1,13 @@
+function myClickEvent() {
+    alert('the buttin was pressed');
+}
+
+function myChange() {
+    let element = document.getElementById('circle');
+    element.style.backgroundColor = 'blue';
+}
+
+function myLoad() {
+    let element = document.getElementById('circle');
+    element.addEventListener('click', myChange);
+}
