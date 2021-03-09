8a2f8bc2e506072419249705bdcaccc34010b019
Eden Brawn <edenbrawn@gmail.com>
2021-03-09 17:36:52 +0000

    Execersise6

--- wk7-git/index.js
+++ wk7-git/index.js
@@ -1,17 +1,25 @@
-function myClickEvent() {
-    alert('the buttin was pressed');
+function myInterval() {
+    let element = document.getElementById('circle');
+    let circleOpacity = parseFloat(element.style.opacity); /*parseFloat retuns a string as a decimal number parseInt returns a whole number*/
+    element.style.opacity = circleOpacity - 0.1;
 }
 
-function myChange() {
-    let element = document.getElementById('circle');
-    let circleOpacity = parseFloat(element.style.opacity);
-    element.style.opacity = circleOpacity - 0.1;
+function myInterval() {
+    setInterval(myClickEvent, 1000);/*1000 = 1 second*/
 }
 
 function myLoad() {
     let element = document.getElementById('circle');
     element.addEventListener('click', myChange);
-    element.style.opacity = '0.5';
+    element.style.opacity = '1';
 }
 
 document.addEventListener('DOMContentLoaded', myLoad);
+/*different events include:
+DOMContentLoaded
+click
+mouseenter
+mouseleave
+keyup
+keydown
+*/
