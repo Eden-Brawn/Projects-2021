ec138de1d80242f74446cb351ae10cdccd52d31b
Eden Brawn <edenbrawn@gmail.com>
2021-03-09 17:07:33 +0000

    Execersise2

--- wk7-git/index.js
+++ wk7-git/index.js
@@ -5,11 +5,13 @@ function myClickEvent() {
 function myChange() {
     let element = document.getElementById('circle');
     element.style.backgroundColor = 'blue';
+    
 }
 
 function myLoad() {
     let element = document.getElementById('circle');
     element.addEventListener('click', myChange);
+    element.addEventListener('click', myClickEvent);
 }
 
 document.addEventListener('DOMContentLoaded', myLoad);
